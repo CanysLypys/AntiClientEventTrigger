fb6119b4481538a5c1f7e721e1c3aa904912702c
Canis_Lupus <56067968+CanysLypys@users.noreply.github.com>
2022-03-05 22:20:27 +0100

    Removed unnecessary code.

--- client/client.lua
+++ client/client.lua
@@ -1,12 +1,3 @@
-local resourceList = {}
-local allResources = GetNumResources()
-for i = 0, allResources, 1 do
-    local resource_name = GetResourceByFindIndex(i)
-    if resource_name and GetResourceState(resource_name) == "started" then
-        table.insert(resourceList, resource_name)
-    end
-end
-
 for k, clientEvents in pairs(Config.ProtectTheseClientEvents) do
     RegisterNetEvent(clientEvents)
     AddEventHandler(clientEvents, function()
